8a15ca36a860582d6b94f03502db348b3a70acca
truscher <thibaut.ruscher@epitech.eu>
2023-07-31 16:30:33 +0200

    feat: link top score back and front

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Navbar from "@/components/NavigationBar/navBar";
 import Menu from "@/components/menu";
+import Top from "@/components/top";
+import ApiService from "@/utils/ApiService";
 
-export default function HomeLayout({
+const fetchTopUsers = async () => {
+  const res = await ApiService.getTopUsers();
+  if (res.status !== 200) return;
+
+  return res.data;
+};
+
+export default async function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const topUsers = await fetchTopUsers();
   return (
     <section>
       <Navbar />
-      <div className="container mx-auto px-0 lg:px-10 lg:pt-8 lg:max-w-screen-xxl">
+      <div className="lg:max-w-screen-xxl container mx-auto px-0 lg:px-10 lg:pt-8">
         <div className="flex">
-          <div className="hidden lg:block px-5">
+          <div className="hidden px-5 lg:block">
             <Menu />
           </div>
           <div className="flex-grow">{children}</div>
+          <div className="hidden px-5 lg:block">
+            <Top topUsers={topUsers} />
+          </div>
         </div>
       </div>
     </section>
